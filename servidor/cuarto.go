cc81e80d9e5f50dcc671d78596c9fd758a8da755
y421 <yaeljuego@live.com.mx>
2022-10-19 15:06:14 -0500

    Al día con main

---
+++ servidor/cuarto.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+		"net"
+)
+
+type Cuarto struct {
+	name   string
+	users  map[net.Addr]string
+}
+
+// NuevoCuarto crea un cuarto y lo devuelve.
+func NuevoCuarto(name string) *Cuarto{
+	return &Cuarto{
+		name: name,
+		users: make(map[net.Addr]string),
+	}
+}
+
+// RecibeMensaje recibe el PUBLICMESSAGEFROM.
+func (c *Cuarto) RecibeMensaje(){
+
+}
+
+// agregaIntegrante agrega al integrante y manda un mensaje de bienvenida.
+func (c *Cuarto) agregaIntegrante(){
+}
+
+// eliminaIntegrante elimina al integrante y manda un mensaje de despedida.
+func (c *Cuarto) eliminaIntegrante(){
+}
---
+++ servidor/cuarto_test.go
@@ -0,0 +1,16 @@
+<<<<<<< HEAD:recursos/cuarto_test.go
+package recursos_test
+=======
+package main
+>>>>>>> feature_servidor:servidor/cuarto_test.go
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+	"encoding/json"
+)
